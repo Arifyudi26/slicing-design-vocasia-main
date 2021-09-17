6249238583c2ce18f71d1e4fd8f57d99c162531b
Muhamad Zulfikri <muhzulfik13@gmail.com>
2021-09-18 00:03:15 +0700

    change layouting, responsive and chart

--- assets/js/pages/dashboard.js
+++ assets/js/pages/dashboard.js
@@ -1,115 +1,190 @@
 var optionsProfileVisit = {
-    annotations: {
-        position: 'right'
+  annotations: {
+    position: "right",
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  chart: {
+    type: "bar",
+    height: "200",
+    width: "100%",
+    toolbar: {
+      show: false,
     },
-    dataLabels: {
-        enabled: false
+  },
+  fill: {
+    opacity: 1,
+  },
+  plotOptions: {},
+  series: [
+    {
+      name: "jumlah siswa baru",
+      data: [350, 150, 300, 450, 600, 350, 150, 300, 450, 600, 100, 150],
     },
-    chart: {
-        type: 'bar',
-        height: '200',
-        width: '100%',
-    },
-    fill: {
-        opacity: 1
-    },
-    plotOptions: {},
-    series: [{
-        name: 'sales',
-        data: [0, 150, 300, 450, 600]
-    }],
-    colors: '#435ebe',
-    xaxis: {
-        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-    },
-}
+  ],
+  colors: "#127CA6",
+  xaxis: {
+    categories: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+  },
+};
+
 let optionsVisitorsProfile = {
-    series: [87, 13],
-    labels: ['24 Kursus', '21 Kursus'],
-    colors: ['#127CA6', '#CD2228'],
-    chart: {
-        type: 'donut',
-        width: '100%',
+  series: [87, 13],
+  labels: ["Jumlah Kursus", "Kursus Aktif"],
+  colors: ["#127CA6", "#CD2228"],
+  chart: {
+    type: "donut",
+  },
+  legend: {
+    position: "right",
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  responsive: [
+    {
+      breakpoint: undefined,
+      options: {},
     },
-    legend: {
-        position: 'right'
+  ],
+  plotOptions: {
+    pie: {
+      donut: {
+        size: "65%",
+        labels: {
+          show: true,
+          name: {
+            show: false,
+          },
+          value: {
+            show: true,
+            fontSize: "22px",
+            fontFamily: "Roboto, sans-serif",
+            fontWeight: "bold",
+            color: undefined,
+          },
+          total: {
+            show: true,
+            showAlways: false,
+            fontSize: "22px",
+            fontFamily: "Roboto, sans-serif",
+            fontWeight: 600,
+          },
+        },
+      },
     },
-    plotOptions: {
-        pie: {
-            donut: {
-                size: '46%'
-            }
-        }
-    }
-}
+  },
+};
 
 var optionsEurope = {
-    series: [{
-        name: 'series1',
-        data: [310, 800, 600, 430, 540, 340, 605, 805, 430, 540, 340, 605]
-    }],
-    chart: {
-        height: 80,
-        type: 'area',
-        toolbar: {
-            show: false,
-        },
+  series: [
+    {
+      name: "series1",
+      data: [310, 800, 600, 430, 540, 340, 605, 805, 430, 540, 340, 605],
     },
-    colors: ['#5350e9'],
-    stroke: {
-        width: 2,
+  ],
+  chart: {
+    height: 80,
+    type: "area",
+    toolbar: {
+      show: false,
     },
-    grid: {
-        show: false,
-    },
-    dataLabels: {
-        enabled: false
-    },
-    xaxis: {
-        type: 'datetime',
-        categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z", "2018-09-19T07:30:00.000Z", "2018-09-19T08:30:00.000Z", "2018-09-19T09:30:00.000Z", "2018-09-19T10:30:00.000Z", "2018-09-19T11:30:00.000Z"],
-        axisBorder: {
-            show: false
-        },
-        axisTicks: {
-            show: false
-        },
-        labels: {
-            show: false,
-        }
-    },
+  },
+  colors: ["#5350e9"],
+  stroke: {
+    width: 2,
+  },
+  grid: {
     show: false,
-    yaxis: {
-        labels: {
-            show: false,
-        },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  xaxis: {
+    type: "datetime",
+    categories: [
+      "2018-09-19T00:00:00.000Z",
+      "2018-09-19T01:30:00.000Z",
+      "2018-09-19T02:30:00.000Z",
+      "2018-09-19T03:30:00.000Z",
+      "2018-09-19T04:30:00.000Z",
+      "2018-09-19T05:30:00.000Z",
+      "2018-09-19T06:30:00.000Z",
+      "2018-09-19T07:30:00.000Z",
+      "2018-09-19T08:30:00.000Z",
+      "2018-09-19T09:30:00.000Z",
+      "2018-09-19T10:30:00.000Z",
+      "2018-09-19T11:30:00.000Z",
+    ],
+    axisBorder: {
+      show: false,
     },
-    tooltip: {
-        x: {
-            format: 'dd/MM/yy HH:mm'
-        },
+    axisTicks: {
+      show: false,
     },
+    labels: {
+      show: false,
+    },
+  },
+  show: false,
+  yaxis: {
+    labels: {
+      show: false,
+    },
+  },
+  tooltip: {
+    x: {
+      format: "dd/MM/yy HH:mm",
+    },
+  },
 };
 
 let optionsAmerica = {
-    ...optionsEurope,
-    colors: ['#008b75'],
-}
+  ...optionsEurope,
+  colors: ["#008b75"],
+};
 let optionsIndonesia = {
-    ...optionsEurope,
-    colors: ['#dc3545'],
-}
+  ...optionsEurope,
+  colors: ["#dc3545"],
+};
 
-
-
-var chartProfileVisit = new ApexCharts(document.querySelector("#chart-profile-visit"), optionsProfileVisit);
-var chartVisitorsProfile = new ApexCharts(document.getElementById('chart-visitors-profile'), optionsVisitorsProfile)
-var chartEurope = new ApexCharts(document.querySelector("#chart-europe"), optionsEurope);
-var chartAmerica = new ApexCharts(document.querySelector("#chart-america"), optionsAmerica);
-var chartIndonesia = new ApexCharts(document.querySelector("#chart-indonesia"), optionsIndonesia);
+var chartProfileVisit = new ApexCharts(
+  document.querySelector("#chart-profile-visit"),
+  optionsProfileVisit
+);
+var chartVisitorsProfile = new ApexCharts(
+  document.getElementById("chart-visitors-profile"),
+  optionsVisitorsProfile
+);
+var chartEurope = new ApexCharts(
+  document.querySelector("#chart-europe"),
+  optionsEurope
+);
+var chartAmerica = new ApexCharts(
+  document.querySelector("#chart-america"),
+  optionsAmerica
+);
+var chartIndonesia = new ApexCharts(
+  document.querySelector("#chart-indonesia"),
+  optionsIndonesia
+);
 
 chartIndonesia.render();
 chartAmerica.render();
 chartEurope.render();
 chartProfileVisit.render();
-chartVisitorsProfile.render()
+chartVisitorsProfile.render();
